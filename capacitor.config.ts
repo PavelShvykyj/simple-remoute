57635caeaf809fe9f06d44eb4fc8ebc3ae305a5c
pavel <pavel.shvydkyi@swansoftwaresolutions.com>
2024-12-07 14:36:31 +0200

    init git

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'simple.remote',
+  appName: 'simple-remote',
+  webDir: 'www'
+};
+
+export default config;
