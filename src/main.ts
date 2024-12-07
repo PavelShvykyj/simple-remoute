daa1b5d97cf171a11e0bdc838cf3626d06a24f92
pavel <pavel.shvydkyi@swansoftwaresolutions.com>
2024-12-07 15:52:37 +0200

    add firebase

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { environment } from 'src/environments/environment';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideRouter(routes,
+       withPreloading(PreloadAllModules)),
+       provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+       provideAuth(() => getAuth()),
+       provideDatabase(() => getDatabase()),
   ],
 });
