778b27ec485b9fcfc1c19566ec7b8dd07fb73d27
pavel <pavel.shvydkyi@swansoftwaresolutions.com>
2024-12-07 16:27:13 +0200

    return env to git flow to built cap

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBRcOZ52d_YPrp2q5XVRYez_xVN1yf5BDI",
+    authDomain: "simple-tech-trek.firebaseapp.com",
+    databaseURL: "https://simple-tech-trek-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "simple-tech-trek",
+    storageBucket: "simple-tech-trek.firebasestorage.app",
+    messagingSenderId: "547594544139",
+    appId: "1:547594544139:web:60eaa1116d11819b6ad30e"
+  },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBRcOZ52d_YPrp2q5XVRYez_xVN1yf5BDI",
+    authDomain: "simple-tech-trek.firebaseapp.com",
+    databaseURL: "https://simple-tech-trek-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "simple-tech-trek",
+    storageBucket: "simple-tech-trek.firebasestorage.app",
+    messagingSenderId: "547594544139",
+    appId: "1:547594544139:web:60eaa1116d11819b6ad30e"
+  },
+}
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
