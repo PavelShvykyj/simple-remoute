9ad1d4e2c8217d60f3fe82feb61524ba04af2d1c
pavel <pavel.shvydkyi@swansoftwaresolutions.com>
2024-12-14 23:03:35 +0200

    toolbars and refresher

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonApp, IonRouterOutlet, IonRefresher, IonRefresherContent } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, IonRefresher, IonRefresherContent],
 })
 export class AppComponent {
-  constructor() {}
+  handleRefresh(event: any) {
+    window.location.reload();
+
+    setTimeout(() => {
+      event.target?.complete();
+    }, 1000);
+  }
 }
