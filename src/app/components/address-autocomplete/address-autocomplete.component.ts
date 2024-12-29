90445793969c3d61de3eee9f85f1ad6a1fd1c4dc
pavel <pavel.shvydkyi@swansoftwaresolutions.com>
2024-12-29 16:47:53 +0200

    detail page form with address component

--- src/app/components/address-autocomplete/address-autocomplete.component.ts
+++ src/app/components/address-autocomplete/address-autocomplete.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit, DestroyRef, inject, } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, OnInit, DestroyRef, inject, input, OnDestroy, signal, viewChild, computed, } from '@angular/core';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import { ControlContainer, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import {
-  IonCard,
   IonInput,
-  IonCardHeader,
-  IonCardTitle,
-  IonCardContent,
   IonItem,
-  IonList,
-  IonButton, IonIcon, IonListHeader } from '@ionic/angular/standalone';
+  IonButton,
+  IonIcon,
+  IonCol,
+  IonGrid,
+  IonRow,
+  IonSelect,
+  IonSelectOption,
+  IonSpinner } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { chevronDownCircle, closeCircle, searchOutline } from 'ionicons/icons';
+import { map } from 'rxjs';
 import { DEFOULT_REGION } from 'src/app/constants/defoult.values';
 
 @Component({
@@ -17,34 +22,57 @@ import { DEFOULT_REGION } from 'src/app/constants/defoult.values';
   templateUrl: './address-autocomplete.component.html',
   styleUrls: ['./address-autocomplete.component.scss'],
   standalone: true,
-  imports: [IonListHeader, IonIcon,
+  viewProviders: [
+    {
+      provide: ControlContainer,
+      useFactory: () => inject(ControlContainer, { skipSelf: true })
+    }
+  ],
+  imports: [
+    IonSelect,
+    IonSelectOption,
+    IonSpinner,
     ReactiveFormsModule,
+    IonRow,
+    IonGrid,
+    IonCol,
+    IonIcon,
     IonButton,
-    IonList,
     IonItem,
-    IonCardContent,
-    IonCardTitle,
-    IonCardHeader,
     IonInput,
-    IonCard,
-
-  ]
+  ],
 })
-export class AddressAutocompleteComponent  implements OnInit {
+export class AddressAutocompleteComponent  implements OnInit, OnDestroy {
   predictions: google.maps.places.AutocompletePrediction[] = [];
   housePredictions: google.maps.places.AutocompletePrediction[] = [];
   destroyRef$ = inject(DestroyRef);
+  parentFormContainer = inject(ControlContainer)
+  controlKey = input.required<string>();
+  controlLabel = input<string>('Address');
+  addressIsSearching = signal(false);
+  selectComponent = viewChild(IonSelect);
 
-
-  private autocompleteService!: google.maps.places.AutocompleteService;
 
   addressForm = new FormGroup({
     country: new FormControl(DEFOULT_REGION.country, { nonNullable: true }),
     region: new FormControl(DEFOULT_REGION.region, { nonNullable: true }),
     city: new FormControl(DEFOULT_REGION.city, { nonNullable: true }),
-    street: new FormControl('', { nonNullable: true, validators: [Validators.required], updateOn: 'blur' }  ),
-    house: new FormControl('', { nonNullable: true, validators: [Validators.required], updateOn: 'blur' }),
+    street: new FormControl('', { nonNullable: true, validators: [Validators.required]}  ),
+    confirmedStreet: new FormControl('', { nonNullable: true, validators: [Validators.required] }  ),
+    house: new FormControl('', { nonNullable: true, validators: [Validators.required] }),
+    flat: new FormControl(0, { nonNullable: true, validators: [Validators.required] }),
   })
+  streetIsComfirmed = toSignal(this.addressForm.get('confirmedStreet')!.valueChanges.pipe(map(value => !!value)), {initialValue: false});
+
+
+
+  private autocompleteService!: google.maps.places.AutocompleteService;
+
+
+
+  get parentFormGroup() {
+    return this.parentFormContainer.control as FormGroup
+  }
 
   get selectedStreet(): string {
     return this.addressForm.get('street')?.value ?? '';
@@ -55,14 +83,26 @@ export class AddressAutocompleteComponent  implements OnInit {
   }
 
 
-  constructor() { }
+  constructor() {
+    addIcons({chevronDownCircle,searchOutline,closeCircle,});
+  }
 
   ngOnInit() {
+    this.parentFormGroup.addControl(this.controlKey(), this.addressForm)
+
     this.autocompleteService = new google.maps.places.AutocompleteService();
 
-    this.addressForm.get('street')?.valueChanges
-    .pipe(takeUntilDestroyed(this.destroyRef$))
-    .subscribe(streetValue => this.onStreetInput(streetValue));
+    this.addressForm.get('street')
+      ?.valueChanges
+      .pipe(takeUntilDestroyed(this.destroyRef$))
+      .subscribe(() => {
+        this.addressForm.get('confirmedStreet')?.setValue('');
+        this.predictions = []
+      });
+  }
+
+  ngOnDestroy() {
+    this.parentFormGroup.removeControl(this.controlKey())
   }
 
   selectStreet(prediction: google.maps.places.AutocompletePrediction) {
@@ -71,11 +111,14 @@ export class AddressAutocompleteComponent  implements OnInit {
   }
 
 
-  onStreetInput(streetValue: string) {
+  onStreetSearch() {
+    this.predictions = [];
     const address = {...this.addressForm.value};
-    address.street = streetValue;
-    console.log('onStreetInput', streetValue)
-    if (address.street) {
+    address.street = address.street ?? '';
+    if (!address.street) {
+      return
+    }
+      this.addressIsSearching.set(true);
       const query = `${address.country}, ${address.region}, ${address.city}, ${address.street}`;
       console.log('query',query)
       this.autocompleteService.getPlacePredictions(
@@ -85,44 +128,36 @@ export class AddressAutocompleteComponent  implements OnInit {
         },
         (predictions, status) => {
           console.log('predictions, status',predictions, status);
+          this.addressIsSearching.set(false);
           if (status === google.maps.places.PlacesServiceStatus.OK) {
             this.predictions = (predictions || []).filter(prediction =>
               prediction.description.includes(address.city!));
+            console.log('field prediction',this.predictions) ;
+            setTimeout(()=>{this.selectComponent()?.open();},0)
           } else {
             this.predictions = [];
           }
         }
       );
-    } else {
-      this.predictions = [];
-    }
-  }
-
-  onHouseInput(event: any) {
-    const address = this.addressForm.value
-
-    if (address.house && address.street) {
-      const query = `${address.house}, ${address.street}`;
-      this.autocompleteService.getPlacePredictions(
-        { input: query, types: ['address'] },
-        (predictions, status) => {
-          if (status === google.maps.places.PlacesServiceStatus.OK) {
-            this.housePredictions = predictions || [];
-          } else {
-            this.housePredictions = [];
-          }
-        }
-      );
-    } else {
-      this.housePredictions = [];
-    }
-  }
-
-  onBlurButtonClick(inputElement: IonInput) {
-    inputElement.getInputElement().then(el=> el.blur());
-  }
-
-  confirmAddress() {
 
   }
+
+  onConfirmedStreetChange(event: any) {
+    this.predictions = [];
+    this.addressForm.get('street')?.setValue(event.detail.value.description);
+    this.addressForm.get('confirmedStreet')?.setValue(event.detail.value.structured_formatting.main_text);
+  }
+
+  onConfirmedStreetCancel() {
+    this.predictions = [];
+    this.addressForm.get('confirmedStreet')?.setValue('');
+  }
+
+  onConfirmedStreetDismiss() {
+    this.predictions = [];
+    this.addressForm.get('confirmedStreet')?.setValue('');
+  }
+
+
+
 }
--- src/app/home/visits/document-detail/document-detail.page.ts
+++ src/app/home/visits/document-detail/document-detail.page.ts
@@ -1,21 +1,60 @@
 import { Component, inject, OnInit, DestroyRef, ViewContainerRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonNavLink, IonBackButton, IonButtons, IonIcon, IonFooter, IonAvatar } from '@ionic/angular/standalone';
+import { ControlContainer, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  IonContent,
+  IonInput,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+  IonNavLink,
+  IonBackButton,
+  IonButtons,
+  IonIcon,
+  IonFooter,
+  IonAvatar,
+  IonCol,
+  IonGrid,
+  IonRow } from '@ionic/angular/standalone';
 import { PricePage } from '../../price/price.page';
 import {
   caretBack, closeOutline, checkmarkOutline } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
-
+import { AddressAutocompleteComponent } from 'src/app/components/address-autocomplete/address-autocomplete.component';
 @Component({
   selector: 'app-document-detail',
   templateUrl: './document-detail.page.html',
   styleUrls: ['./document-detail.page.scss'],
   standalone: true,
-  imports: [IonIcon, IonAvatar, IonFooter,  IonButtons, IonBackButton, IonNavLink, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, ReactiveFormsModule]
+  imports: [
+    IonRow,
+    IonGrid,
+    IonCol,
+    IonIcon,
+    IonAvatar,
+    IonFooter,
+    IonButtons,
+    IonInput,
+    IonBackButton,
+    IonNavLink,
+    IonButton,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AddressAutocompleteComponent]
 })
 export class DocumentDetailPage  {
   priceDetail = PricePage;
+  documentHeaderForm = new FormGroup({
+    comment: new FormControl('', { nonNullable: true }),
+    docDate: new FormControl(new Date(), { nonNullable: true })
+  })
+
   constructor() {
     addIcons({closeOutline,checkmarkOutline,caretBack});
   }
